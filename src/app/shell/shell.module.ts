18bca76e651d9cabc6847df5f9db062953d90d53
Yordanis AL <yordanis.lx@gmail.com>
2022-03-19 13:32:50 -0400

    remove unnecesary code

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -4,14 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ShellRoutingModule } from './shell-routing.module';
 import { ShellMainPageComponent } from './pages/shell-main/shell-main-page.component';
 import { ShellHeaderComponent } from './components/shell-header/shell-header.component';
-import { ShellMenuComponent } from './components/shell-menu/shell-menu.component';
-
 
 @NgModule({
   declarations: [
     ShellMainPageComponent,
-    ShellHeaderComponent,
-    ShellMenuComponent
+    ShellHeaderComponent
   ],
   imports: [
     CommonModule,
